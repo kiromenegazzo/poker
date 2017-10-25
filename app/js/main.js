f2ae98f04a4139101635914d81cd4d84cd34d449
Kirill <html@i-vengo.com>
2017-10-25 18:46:17 +0300

    add aria attributes

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,10 @@
 
 const Poker = function() {
 
-    const self = this;
-
     this.popup = document.querySelector('.popup');
     this.popupClose = document.querySelector('.popup__close');
     this.cardList = document.querySelector('.card__list');
+    this.isMobile = (navigator.userAgent.match(/iPad|iPhone|Mobile|Android|Windows Phone/g) ? true : false);
     this.loadIndex = 1;
 
     this.combName = [
@@ -35,32 +34,6 @@ const Poker = function() {
         'ни одна из вышеописанных комбинаций'
     ];
 
-    this.setCardSize = () =>  {
-        let cards = self.cardList.children;
-        let containerHeight = document.querySelector('.container').clientHeight;
-        let rows = window.innerWidth < window.innerHeight ? 5 : 2;
-
-        [].forEach.call(cards, card => {
-            card.style.cssText = `height:${containerHeight / rows}px`;
-        });
-    };
-
-    this.showPopup = index => {
-        const popupImage = document.querySelector('.popup__image');
-        const popupTitle = document.querySelector('.popup__title');
-        const popupDesc = document.querySelector('.popup__desc');
-
-        self.popup.classList.add('popup--open');
-
-        popupImage.style.backgroundImage = `url(img/hand-${index}.png)`;
-        popupTitle.innerText = `${self.combName[index - 1]}`;
-        popupDesc.innerText = `${self.combDesc[index - 1]}`;
-    };
-
-    this.hidePopup = () => {
-        self.popup.classList.remove('popup--open');
-    };
-
     this.showPercent = (counter, number) => {
         let percent = Math.round(counter/number * 100);
         const preload = document.querySelector('.preload');
@@ -73,26 +46,87 @@ const Poker = function() {
 
         if(counter == number){
             document.body.removeChild(preload);
-            self.setCardSize();
+            this.setCardSize();
         }
     };
 
-    this.setTransformOrigin = (transformOriginX, transformOriginY) => {
-        self.popup.style.cssText = `-webkit-transform-origin: ${transformOriginX}% ${transformOriginY}% 0;
-                                    transform-origin: ${transformOriginX}% ${transformOriginY}% 0;`;
+    this.setCardSize = () =>  {
+        let cards = this.cardList.children;
+        let containerHeight = document.querySelector('.container').clientHeight;
+        let rows = window.innerWidth < window.innerHeight ? 5 : 2;
 
+        [].forEach.call(cards, card => {
+            card.style.cssText = `height:${containerHeight / rows}px`;
+        });
+    };
+
+    this.setPopupContent = index => {
+        const popupImage = document.querySelector('.popup__image use');
+        const popupTitle = document.querySelector('.popup__title');
+        const popupDesc = document.querySelector('.popup__desc');
+
+        //popupImage.style.backgroundImage = `url(img/hand-${index}.png)`;
+        popupImage.setAttribute('href', `img/sprite.svg#1`);
+        //popupImage.setAttribute('href', `img/sprite.svg#${index}`);
+        popupTitle.innerText = `${this.combName[index - 1]}`;
+        popupDesc.innerText = `${this.combDesc[index - 1]}`;
+    };
+
+    this.openPopup = () => {
+        if(this.focusedEl !== null) this.popupClose.focus();
+        this.popup.classList.add('popup--open');
+    };
+
+    this.closePopup = () => {
+        if(this.focusedEl !== null) this.focusedEl.focus();
+        this.popup.classList.remove('popup--open');
+    };
+
+    this.setTransformOrigin = (x, y) => {
+        this.popup.style.cssText = `-webkit-transform-origin: ${x}% ${y}% 0;
+                                    transform-origin: ${x}% ${y}% 0;`;
+
+    };
+
+    this.buildPopup = () => {
+        let x = Math.round(event.pageX / window.innerWidth * 100) || '50';
+        let y = Math.round(event.pageY / window.innerHeight * 100) || '50';
+        let cardItem = event.target;
+
+        if(event.target.tagName == 'svg' || event.target.className == 'card__title'){
+            cardItem = event.target.parentNode;
+        } else if(event.target.tagName == 'use') {
+            cardItem = event.target.parentNode.parentNode;
+        }
+
+        this.setTransformOrigin(x, y);
+        this.setPopupContent(cardItem.getAttribute('data-card'));
+        this.openPopup();
+        this.focusedEl = !this.isMobile ? cardItem : null;
+    };
+
+    this.actionHandler = () => {
+        const eventType = event.type;
+
+        if (eventType === 'keydown') {
+            if (event.keyCode === 13 || event.keyCode === 32) {
+                this.buildPopup();
+            }
+        } else if (eventType === 'click') {
+            this.buildPopup();
+        }
     };
 
     this.loadImage = () => {
         return new Promise((resolve, reject) => {
             let image = new Image();
-            image.src = `img/hand-${self.loadIndex}.png`;
+            image.src = `img/hand-${this.loadIndex}.png`;
             image.onload = () => {
-                self.showPercent(self.loadIndex, self.combName.length);
+                this.showPercent(this.loadIndex, this.combName.length);
                 resolve(image.src);
-                if(self.loadIndex !== self.combName.length) {
-                    self.loadIndex++;
-                    self.loadImage();
+                if(this.loadIndex !== this.combName.length) {
+                    this.loadIndex++;
+                    this.loadImage();
                 }
 
             };
@@ -102,16 +136,16 @@ const Poker = function() {
 
     this.preloadImages = () => {
         let images = [];
-        let imagesNumber = self.combName.length, imagesCounter = 0;
+        let imagesNumber = this.combName.length, imagesCounter = 0;
 
         for(let n = 1; n < imagesNumber + 1; n++) {
             images[n] = new Image();
             images[n].src = `img/hand-${n}.png`;
             images[n].onload = () => {
                 imagesCounter++;
-                self.showPercent(imagesCounter, imagesNumber);
+                this.showPercent(imagesCounter, imagesNumber);
                 if(imagesCounter == imagesNumber) {
-                    self.setCardSize();
+                    this.setCardSize();
                 }
             };
         }
@@ -127,27 +161,24 @@ const Poker = function() {
         }
     };
 
-    this.popupClose.addEventListener('click', self.hidePopup);
-    this.cardList.addEventListener('click', e => {
-        let x = Math.round(e.pageX / window.innerWidth * 100);
-        let y = Math.round(e.pageY / window.innerHeight * 100);
-        let cardItem = e.target;
+    this.popupClose.addEventListener('click', this.closePopup);
+    this.cardList.addEventListener('click', this.actionHandler);
+    this.cardList.addEventListener('keydown', this.actionHandler);
 
-        if(e.target.className == 'card__image' || e.target.className == 'card__title'){
-            cardItem = e.target.parentNode;
-        }
-
-        self.setTransformOrigin(x, y);
-        self.showPopup(cardItem.getAttribute('data-index'));
+    this.popup.addEventListener('keydown', () => {
+       if(event.keyCode == '9') {
+            event.preventDefault();
+       } else if(event.keyCode == '27') {
+           this.closePopup();
+       }
     });
-    window.addEventListener('resize', self.setCardSize);
-    window.applicationCache.addEventListener('updateready', self.checkUpdate);
+
+    window.addEventListener('resize', this.setCardSize);
+    window.applicationCache.addEventListener('updateready', this.checkUpdate);
 };
 
 window.addEventListener('DOMContentLoaded', () => {
     const poker = new Poker();
 
     window.Promise ? poker.loadImage() : poker.preloadImages();
-
-
 });
---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+var gulp      = require('gulp'), // Подключаем Gulp
+    less        = require('gulp-less'), //Подключаем less пакет,
+    browserSync = require('browser-sync'), // Подключаем Browser Sync
+    concat      = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+    uglify      = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
+    cssnano     = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
+    rename      = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+    del         = require('del'), // Подключаем библиотеку для удаления файлов и папок
+    autoprefixer = require('gulp-autoprefixer'),// Подключаем библиотеку для автоматического добавления префиксов
+    babel = require('gulp-babel'),
+    fs = require('fs-extra');
+
+var manifestData =
+`CACHE MANIFEST
+# v.${+Date.now()}\n`;
+
+gulp.task('less', function(){ // Создаем таск less
+    return gulp.src('app/less/**/*.less') // Берем источник
+        .pipe(less()) // Преобразуем less в CSS посредством gulp-less
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({stream: true})); // Обновляем CSS на странице при изменении
+});
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: 'app' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([ // Берем все необходимые библиотеки
+            'app/js/main.js'
+        ])
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concat('main.min.js')) // Собираем их в кучу в новом файле libs.min.js
+        .pipe(uglify()) // Сжимаем JS файл
+        .pipe(gulp.dest('app/js')); // Выгружаем в папку dist
+});
+
+gulp.task('css-libs', ['less'], function() {
+    return gulp.src('app/css/styles.css') // Выбираем файл для минификации
+        .pipe(cssnano({
+            autoprefixer: {
+                browsers:['ios > 8'],
+                add: true
+            }
+        })) // Сжимаем
+        .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+        .pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
+});
+
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
+    gulp.watch('app/less/**/*.less', ['less']); // Наблюдение за less файлами в папке less
+    gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
+});
+
+gulp.task('read-dir', function() {
+    fs.readdir('docs/img', function(err, items) {
+        for (var i=0; i<items.length; i++) {
+            manifestData += `img/${items[i]} \n`;
+
+        }
+    });
+    fs.readdir('docs/', function(err, items) {
+        for (var i=0; i<items.length; i++) {
+            if(items[i] !== 'img' && items[i] !== 'cache.manifest') {
+                manifestData += `./${items[i]} \n`;
+                if(i == items.length - 1) {
+                    gulp.start('update-manifest');
+                }
+            }
+        }
+    });
+});
+
+gulp.task('update-manifest', function(){
+    fs.writeFile('docs/cache.manifest', manifestData);
+});
+
+gulp.task('build', ['less', 'scripts', 'read-dir'], function() {
+
+    gulp.src('app/img/*').pipe(gulp.dest('docs/img'));
+});
+
+gulp.task('default', ['watch']);
