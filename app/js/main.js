90009fffa154152125c571f6504b060cbe742ab1
Kirill <html@i-vengo.com>
2017-11-01 15:57:14 +0300

    svg

--- app/js/main.js
+++ app/js/main.js
@@ -2,74 +2,42 @@
 
 const Poker = function() {
 
+    this.body = document.body;
     this.popup = document.querySelector('.popup');
     this.popupClose = document.querySelector('.popup__close');
     this.cardList = document.querySelector('.card__list');
     this.isMobile = (navigator.userAgent.match(/iPad|iPhone|Mobile|Android|Windows Phone/g) ? true : false);
-    this.loadIndex = 1;
 
-    this.combName = [
-        'Роял Стрит Флэш',
-        'Стрит Флэш',
-        'Каре',
-        'Фулл хаус',
-        'Флеш',
-        'Стрит',
-        'Тройка',
-        'Две пары',
-        'Пара',
-        'Старшая карта'
-    ];
-
-    this.combDesc = [
-        '5 старших карт одной масти',
-        'любые пять карт одной масти по порядку',
-        'четыре карты одного достоинства',
-        'одна тройка и одна пара',
-        'пять карт одной масти',
-        'пять карт по порядку любых мастей',
-        'три карты одного достоинства',
-        'две пары карт одного достоинства',
-        'две карты одного достоинства',
-        'ни одна из вышеописанных комбинаций'
-    ];
-
-    this.showPercent = (counter, number) => {
-        let percent = Math.round(counter/number * 100);
-        const preload = document.querySelector('.preload');
+    this.showPercent = (current, total) => {
+        let percent = Math.round(current/total * 100);
         const preloadPercent = document.querySelector('.preload__percent');
         const preloadLine = document.querySelector('.preload__line');
 
         preloadPercent.setAttribute('data-percent', percent);
         preloadLine.style.cssText = `-webkit-transform: translate3d(-${100 - percent}%,0,0);
                                      transform: translate3d(-${100 - percent}%,0,0)`;
-
-        if(counter == number){
-            document.body.removeChild(preload);
-            this.setCardSize();
-        }
     };
 
     this.setCardSize = () =>  {
-        let cards = this.cardList.children;
-        let containerHeight = document.querySelector('.container').clientHeight;
+        const cards = document.getElementsByClassName('card__item');
+        const container = document.querySelector('.container');
+        let containerHeight = container.clientHeight;
         let rows = window.innerWidth < window.innerHeight ? 5 : 2;
 
         [].forEach.call(cards, card => {
-            card.style.cssText = `height:${containerHeight / rows}px`;
+            card.style.height = `${containerHeight / rows}px`;
         });
     };
 
     this.setPopupContent = index => {
         const popupImage = document.querySelector('.popup__image use');
         const popupTitle = document.querySelector('.popup__title');
+        const cardTitle = document.getElementsByClassName('card__title')[index - 1];
         const popupDesc = document.querySelector('.popup__desc');
 
-        //popupImage.style.backgroundImage = `url(img/hand-${index}.png)`;
-        popupImage.setAttribute('href', `img/sprite.svg#1`);
-        //popupImage.setAttribute('href', `img/sprite.svg#${index}`);
-        popupTitle.innerText = `${this.combName[index - 1]}`;
-        popupDesc.innerText = `${this.combDesc[index - 1]}`;
+        popupImage.setAttribute('xlink:href', `#${index}`);
+        popupTitle.innerText = `${cardTitle.innerText}`;
+        popupDesc.innerText = `${cardTitle.getAttribute('data-desc')}`;
     };
 
     this.openPopup = () => {
@@ -117,38 +85,35 @@ const Poker = function() {
         }
     };
 
-    this.loadImage = () => {
-        return new Promise((resolve, reject) => {
-            let image = new Image();
-            image.src = `img/hand-${this.loadIndex}.png`;
-            image.onload = () => {
-                this.showPercent(this.loadIndex, this.combName.length);
-                resolve(image.src);
-                if(this.loadIndex !== this.combName.length) {
-                    this.loadIndex++;
-                    this.loadImage();
-                }
+    this.setHref = () => {
+        const images = document.querySelectorAll('.card__image use');
+        const preload = document.querySelector('.preload');
 
-            };
-            image.onerror = () => reject();
+        [].forEach.call(images, (image, i) => {
+            image.setAttribute('xlink:href', `#${i + 1}`);
         });
+
+        this.body.removeChild(preload);
+        this.setCardSize();
     };
 
-    this.preloadImages = () => {
-        let images = [];
-        let imagesNumber = this.combName.length, imagesCounter = 0;
+    this.preloadSprite = () => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', 'img/sprite.svg', true);
+        xhr.send();
 
-        for(let n = 1; n < imagesNumber + 1; n++) {
-            images[n] = new Image();
-            images[n].src = `img/hand-${n}.png`;
-            images[n].onload = () => {
-                imagesCounter++;
-                this.showPercent(imagesCounter, imagesNumber);
-                if(imagesCounter == imagesNumber) {
-                    this.setCardSize();
-                }
-            };
-        }
+        xhr.onprogress = () => {
+            if(event.lengthComputable) {
+                this.showPercent(event.loaded, event.total);
+            }
+        };
+
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                this.body.innerHTML += event.target.response;
+                this.setHref();
+            }
+        };
     };
 
     this.checkUpdate = () => {
@@ -173,12 +138,12 @@ const Poker = function() {
        }
     });
 
+    this.setCardSize();
     window.addEventListener('resize', this.setCardSize);
     window.applicationCache.addEventListener('updateready', this.checkUpdate);
 };
 
 window.addEventListener('DOMContentLoaded', () => {
     const poker = new Poker();
-
-    window.Promise ? poker.loadImage() : poker.preloadImages();
+    poker.preloadSprite();
 });
